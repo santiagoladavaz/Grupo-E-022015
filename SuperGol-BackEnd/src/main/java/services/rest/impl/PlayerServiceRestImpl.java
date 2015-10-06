dca381a5316f05248fbbd7fc513629fb43721bea
matlock <jfflores90@gmail.com>
2015-10-06 16:50:52 -0300

    se modifican archivos de configuracion

---
+++ SuperGol-BackEnd/src/main/java/services/rest/impl/PlayerServiceRestImpl.java
@@ -0,0 +1,48 @@
+package services.rest.impl;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import services.interfaces.PlayerService;
+
+@Service
+@Path("/playerService")
+public class PlayerServiceRestImpl {
+
+	@Autowired
+	private PlayerService playerService;
+
+	
+	
+	@GET
+	@Path("/player/{id}")
+	@Produces("application/json")
+	public int getPlayer(@PathParam("id") final int id) {
+	      return 1;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public PlayerService getPlayerService() {
+		return playerService;
+	}
+
+	public void setPlayerService(PlayerService playerService) {
+		this.playerService = playerService;
+	}	
+	
+}
